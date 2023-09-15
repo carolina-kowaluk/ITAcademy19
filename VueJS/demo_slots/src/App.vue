53971b6a58d5c151cf544cc2675a135bd931eecf
carolina-kowaluk <carolina.kowaluk@gmail.com>
2023-09-15 16:49:39 -0300

    exemplo de slots

---
+++ VueJS/demo_slots/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import BotaoColorido from "./components/BotaoColorido.vue";
+import LayoutBasico from "./components/LayoutBasico.vue";
+</script>
+
+<template>
+  <LayoutBasico>
+    <template #cabecalho>
+      <h1>Exemplos de Slots</h1>
+    </template>
+    <template #default>
+      <button type="button">Clique</button>
+      <BotaoColorido>Clique Aqui</BotaoColorido>
+      <BotaoColorido>
+        <span>Curtir</span>🤍
+      </BotaoColorido>
+    </template>
+    <template #rodape>
+      <p>Desenvolvimento</p>
+    </template>
+  
+  </LayoutBasico>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ VueJS/demo_slots/src/components/BotaoColorido.vue
@@ -0,0 +1,26 @@
+<template>
+    <button type="button" class="botao-colorido">
+        <slot>Botão</slot>
+        
+    </button>
+
+
+</template>
+
+
+<style scoped>
+    .botao-colorido {
+        color:white;
+        background: linear-gradient(315deg, #854997 25%, #5250b4);
+        border: none;
+        padding: 5px 10px;
+        margin: 5px;
+        border-radius: 8px;
+        cursor: pointer;
+    }
+
+    .botao-colorido:hover {
+        background: linear-gradient(315deg, #4c2956, #353475); /* Cor que deseja quando o cursor passar por cima */
+    } 
+
+</style>
