477897f4f81dbbfa202fc508b2315dad5fa2463b
carolina-kowaluk <carolina.kowaluk@gmail.com>
2023-09-18 15:03:58 -0300

    eventos

---
+++ VueJS/demo_eventos/src/components/MeuComponente.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const emit = defineEmits(['evento']);
+
+</script>
+
+<template>
+    <h2>Meu Componente</h2>
+    <button type="button" @click="emit('evento')">Dispare Evento</button>
+
+</template>
