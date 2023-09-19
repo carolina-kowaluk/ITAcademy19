de6db8c2a5d2e5f51f07c071289d95d5abd349a4
carolina-kowaluk <carolina.kowaluk@gmail.com>
2023-09-19 11:00:09 -0300

    finalizando aula 5 vuejs

--- VueJS/demo_vuerouter/src/App.vue
+++ VueJS/demo_vuerouter/src/App.vue
@@ -5,8 +5,8 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <RouterLink to="/node_modules">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about/teste">About</RouterLink>
     </nav>
   </header>
   <RouterView/>
@@ -18,7 +18,7 @@ header {
   max-height: 100vh;
 }
 
-.logo {
+.logo { 
   display: block;
   margin: 0 auto 2rem;
 }
@@ -48,30 +48,4 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- VueJS/demo_vuerouter/src/views/AboutView.vue
+++ VueJS/demo_vuerouter/src/views/AboutView.vue
@@ -1,15 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+const route = useRoute();
+const parametro = route.params.nome;
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About page</h1>
+    <p>{{ parametro }}</p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
